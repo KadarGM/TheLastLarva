6aa18b358fc0d9f4b7b9ecdce7001498935a0669
KadarGM <jackob.zummer@gmail.com>
2025-07-17 21:13:23 +0200

    recreated animations without skeleton

--- Assets/scripts/characterManager.gd
+++ Assets/scripts/characterManager.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-@onready var animation_player = $AnimationPlayer
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var body: Node2D = $Body
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var is_fliped = false
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -19,10 +19,19 @@ func _physics_process(delta):
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		print(direction)
 		animation_player.play("Walk")
 		velocity.x = direction * SPEED
 	else:
 		animation_player.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	flip_body(direction)
 	move_and_slide()
+
+func flip_body(direction) -> void:
+	
+	if direction == -1.0:
+		scale.x = -1.0
+	else:
+		scale.x = 1.0
